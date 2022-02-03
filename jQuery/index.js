2f2fd6751dea850c03bcd59708b2dd87a6229296
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-02-03 22:17:36 +0700

    extra j query

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,28 @@
-$("h1").css("color", "red");
+
+$("#title").addClass('big-with-color');
+$("button").css("background-color", "red");
+
+let originText = $("#title").text();
+
+$(document).keydown(function (e) { 
+    $("#title").text(e.key);
+});
+$(document).keyup(function (e) { 
+    $("#title").text(originText);
+});
+
+$("button").click(function (e) { 
+    console.log("Button clicked");
+    // $("#title").toggle();
+    // $("#title").slideToggle();
+    $("#title").animate({opacity: 0});
+
+});
+
+// $("button").on("click", function () {
+    
+// });
+
+$("#title").before("<button>Hi</button>");
+$("#title").prepend("<button>Bye</button>");
+
