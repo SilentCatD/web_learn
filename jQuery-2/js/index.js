fc7f7a7b57409eff31b99145c987fb920682fd86
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-02-03 23:02:14 +0700

    section 2

--- jQuery-2/js/index.js
+++ jQuery-2/js/index.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    $("body").append("<h1>I love jQuery! </h1>");
+// $("h1:first").css("border", "3px solid green");
+// $("h1:last").css("border", "3px solid red");
+
+// let newText = $("<p>")
+// newText.append("<h1>I just replaced some text.</h1>")
+// $("#textBox").html(newText);
+
+let originText = $("#textBox").text();
+
+$("#textBox").click(function (e) { 
+    $("#textBox").html("<h1>You Clicked Me</h1>");
+});
+
+$("#textBox").mouseleave(function () { 
+    $("#textBox").html(originText);
 });
